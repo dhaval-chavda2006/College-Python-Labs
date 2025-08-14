ca3a27b718c7295e1c6e178282e4d65212797f2a
dhaval-chavda2006 <dhavalchavda866@gmail.com>
2025-08-14 21:23:20 +0530

    Add files via upload

---
+++ postlab5.py
@@ -0,0 +1,69 @@
+# Write a Python program to Count the occurrences of an element in a tuple.
+
+# my_tuple = (1, 2, 3, 2, 2, 4, 5)
+# element = 2
+# occurrences = my_tuple.count(element)
+# print(f"{element} appears {occurrences} times in the tuple.")
+
+
+
+#Python program to Check if an element exists in a tuple.
+# sample_tuple = (1, 2, 3, 4, 5)
+# element_to_check = 3
+
+# if element_to_check in sample_tuple:
+#     print(f"The element {element_to_check} exists in the tuple.")
+# else:
+#     print(f"The element {element_to_check} does not exist in the tuple.")
+
+
+
+
+#Python program to Convert a tuple to a string.
+
+# sample_tuple = ('H', 'e', 'l', 'l', 'o')
+# result_string = str(sample_tuple)
+
+# print("The string is:", result_string)
+
+
+
+
+#Python program to Find the maximum and minimum elements in a tuple.
+
+# sample_tuple = (10, 22, 3, 45, 67, 5, 0)
+
+# maximum = max(sample_tuple)
+# minimum = min(sample_tuple)
+
+# print(f"The maximum element in the tuple is: {maximum}")
+# print(f"The minimum element in the tuple is: {minimum}")
+
+
+
+
+#Python program to convert a tuple of strings to a single string
+sample_tuple = ('Python', 'is', 'awesome')
+result_string = f"{sample_tuple[0]} {sample_tuple[1]} {sample_tuple[2]}"
+print("The single string is:", result_string)
+
+
+
+
+# Python program to sort a tuple of integers using inbuilt function
+# sample_tuple = (5, 2, 9, 1, 7)
+# sorted_tuple = tuple(sorted(sample_tuple))
+# print("Sorted tuple:", sorted_tuple)
+
+
+
+
+
+# Python program to find the first and last elements of a tuple using inbuilt functions
+
+sample_tuple = (10, 20, 30, 40, 50)
+first_element = sample_tuple[0]
+last_element = sample_tuple[-1]
+
+print("The first element is:", first_element)
+print("The last element is:", last_element)
